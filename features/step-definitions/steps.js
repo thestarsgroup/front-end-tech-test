3db46b2d831a280da240ae5c2120fd1a072bd62d
Pooja Chawla <pooja.chawla88@gmail.com>
2023-05-16 12:57:41 +0100

    step definitions and glue code added

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,22 +1,125 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 
-const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
+var assert = require('assert')
+
+const HomePage = require('../pageobjects/home.page');
+const LoginPrompt = require('../pageobjects/login.prompt')
+const JoinPrompt = require('../pageobjects/join.prompt')
+
+
 
 const pages = {
-    login: LoginPage
+    //login: LoginPage
 }
 
-Given(/^I am on the (\w+) page$/, async (page) => {
-    await pages[page].open()
-});
+Given('I am on the home page', function () {
+    HomePage.open();
+    browser.pause(3000);
 
-When(/^I login with (\w+) and (.+)$/, async (username, password) => {
-    await LoginPage.login(username, password)
-});
+  });
 
-Then(/^I should see a flash message saying (.*)$/, async (message) => {
-    await expect(SecurePage.flashAlert).toBeExisting();
-    await expect(SecurePage.flashAlert).toHaveTextContaining(message);
-});
 
+  When('I wait for the page to load', function () {
+    browser.maximizeWindow();
+
+    browser.pause(5000);
+  });
+
+
+
+  Then('the page should load without any errors', function () {
+    HomePage.verifyHomePageLoadsCorrectly();    
+    browser.pause(3000);
+
+
+  });
+
+
+  When('I click the login button', function () {
+    HomePage.clickLoginButton();
+    browser.pause(6000);
+ 
+  });
+
+
+  When('I click the join button', function () {
+    HomePage.clickJoinButton();
+    browser.pause(6000);
+ 
+  });
+
+
+
+  Then('the login prompt should be displayed', function () {
+    LoginPrompt.validateLoginPrompt();
+    LoginPrompt.close();
+
+    });
+
+    Then('the join prompt should be displayed', function () {
+    JoinPrompt.validateJoinPrompt();
+    JoinPrompt.close();
+
+    });
+
+
+    
+    Then('I check all links on the page are working', function (){
+     HomePage.verifyLinksAreWorkingCorrectly();
+    });
+
+
+    Then('the website should use HTTPS',  function () {
+        HomePage.verifyWebsiteSecurity();
+    });
+    
+    
+
+    When('I verify that the sports and events are displayed',  function () {
+        HomePage.verifySportsAndEvents();
+    });
+
+
+    Then('the chosen sports and events should be displayed on In play section',  function () {
+        HomePage.chooseSportsInPlaySection();
+    });
+
+
+
+    When('I select Football from the sports list', function () {
+        HomePage.chooseSportsInPlaySection();
+      });
+      
+    When('I select first match from the event list', function () {
+        HomePage.chooseMatch();
+    });      
+    
+    When('I select The Draw as from the Match odds of betting options', function () {
+        HomePage.chooseMatchOdds();
+    });
+
+    When('I enter a stake of 10 in the betting slip', function () {
+        HomePage.enterStake();
+    });
+
+
+    Then('the bet slip should display 1 bet', function () {
+        HomePage.verifyBetSlipCount();
+    });
+    
+
+    Then('the bet slip should display The Draw betting option', function () {
+        HomePage.verifyOdds();
+    });
+
+    Then('the bet slip should display 10 stake', function () {
+        HomePage.verifyStake();
+    });
+
+    When('I click on the Delete button', function () {
+        HomePage.deleteBetStake();
+    });
+
+    Then('the bet slip should be empty', function () {
+        HomePage.verifyEmptyBetSlip();
+    });
